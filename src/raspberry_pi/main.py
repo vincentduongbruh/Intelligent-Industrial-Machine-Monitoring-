b0e5cf13582f907e16f1db460d0878f7361b4db0
vincentduongbruh <vincent_duong@berkeley.edu>
2025-12-03 02:30:18 -0800

    implemented FOC equations

--- src/raspberry_pi/main.py
+++ src/raspberry_pi/main.py
@@ -1,6 +1,8 @@
 import asyncio
 import struct
+import math
 from bleak import BleakClient, BleakScanner
+
 
 SERVICE_UUID = "8f3eec84-a3cd-4991-9f84-b6d6915e7382"
 CHAR_UUID = "488147e4-8512-4bca-b218-0b84f2f76853"
@@ -8,6 +10,21 @@ CHAR_UUID = "488147e4-8512-4bca-b218-0b84f2f76853"
 def callback_handler(sender, data):
     ax, ay, az, temp = struct.unpack('<ffff', data)
     print(f"Received -> Ax: {ax:.2f}, Ay: {ay:.2f}, Az: {az:.2f}, Temp: {temp:.2f}")
+
+#equation 3.2
+def direct_axis_current(i_a, i_b, i_c):
+    i_d = (math.sqrt(2/3) * i_a) - (i_b / math.sqrt(6)) - (i_c / math.sqrt(6))
+    return i_d
+    
+#equation 3.3
+def quadrature_axis_current(i_a, i_b, i_c, theta):
+    i_q = (i_b / math.sqrt(2)) - (i_c / math.sqrt(2))
+    return i_q
+
+def park_vector_modulus(i_d, i_q):
+    PVM = math.sqrt((i_d ** 2) + (i_q ** 2))
+    return PVM
+
 
 async def main():
     print("Scanning for ESP32")
