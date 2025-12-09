87425ba82a4e0149c7907c53487d2608c2e8207d
nickkbuii <nicholasqb01@gmail.com>
2025-12-09 14:15:41 -0800

    Testing ESP1

--- src/esp1/main/main.ino
+++ src/esp1/main/main.ino
@@ -1,16 +1,16 @@
 #include "MPU9250.h"
 #include "SHT30.h"
-#include "BluetoothHandler.h"
+// #include "BluetoothHandler.h"
 
 MPU9250 imu1(0x68);
 MPU9250 imu2(0x69);
-SHT30 sht;
+// SHT30 sht;
 
-BluetoothHandler btHandler(
-    "ESP32_1", // Device name
-    "8f3eec84-a3cd-4991-9f84-b6d6915e7382",  // Service UUID
-    "488147e4-8512-4bca-b218-0b84f2f76853"   // Characteristic UUID
-);
+// BluetoothHandler btHandler(
+//     "ESP32_1", // Device name
+//     "8f3eec84-a3cd-4991-9f84-b6d6915e7382",  // Service UUID
+//     "488147e4-8512-4bca-b218-0b84f2f76853"   // Characteristic UUID
+// );
 
 SensorPacket currentData;
 
@@ -24,9 +24,9 @@ void setup() {
     imu2.begin();
     imu2.calibrate(1000);ß
 
-    sht.begin();
+    // sht.begin();
 
-    btHandler.begin();
+    // btHandler.begin();
 }
 
 void loop() {
@@ -41,21 +41,21 @@ void loop() {
     float ay = 0.5 * (ay1 + ay2);
     float az = 0.5 * (az1 + az2);
 
-    float temp;
-    bool ok = sht.read(temp);
-    if (ok) {
-        lastTemp = temp;
-    }
+    // float temp;
+    // bool ok = sht.read(temp);
+    // if (ok) {
+    //     lastTemp = temp;
+    // }
 
-    currentData.ax = static_cast<float>(ax);
-    currentData.ay = static_cast<float>(ay);
-    currentData.az = static_cast<float>(az);
-    currentData.temp = lastTemp;
+    // currentData.ax = static_cast<float>(ax);
+    // currentData.ay = static_cast<float>(ay);
+    // currentData.az = static_cast<float>(az);
+    // currentData.temp = lastTemp;
 
     Serial.printf("ax:%d ay:%d az:%d temp:%f\n",
                   ax, ay, az, lastTemp);
 
-    btHandler.notifySensorData(currentData);
+    // btHandler.notifySensorData(currentData);
 
     delay(100);
 }
